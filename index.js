b80dabd49403247c578f4728087669345b45b673
le manh hai <145058458+leviathan4678@users.noreply.github.com>
2025-06-23 03:52:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = 6969;
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/reports', require('./routes/reports'));
